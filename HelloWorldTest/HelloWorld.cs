6f04cfc98eb5bdd3eba7349da6c140cc4c61f1f2
jyang <tjyjpicture@126.com>
2019-03-25 17:01:37 +0800

    Add project files.

---
+++ HelloWorldTest/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class HelloWorld
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World",Program.createMessage());
+        }
+    }
+}
